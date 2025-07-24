31dc0c1472b53625731d45bfb92e42b8d423e7e5
Helena Ruiz <hruizrmz@gmail.com>
2025-07-24 14:26:51 -0600

    Replace user IDs with names
    + better table formatting when exporting to csv
    + comments for code understanding
    + added a .env example

--- scripts/convos.py
+++ scripts/convos.py
@@ -1,10 +1,10 @@
 #
 # Versión 0.1
-# Fecha: 21 de julio de 2025
+# Fecha: 24 de julio de 2025
 #
 # Autor: Helena Ruiz Ramírez
 # Función: Llamar registros de conversaciones de la plataforma LiveConnect según ciertos parametros
-#           de tiempos y etiquetas, para luego exportarlos en un archivo.
+#           de tiempos y etiquetas, para luego exportarlos a un archivo .csv
 #
 import os
 import json
@@ -13,21 +13,24 @@ from dotenv import load_dotenv
 import requests
 import pandas as pd
 
-load_dotenv()
-
+# Quita los límites de las tablas
 pd.set_option('display.max_colwidth', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_columns', None)
 
+# Carga el archivo .env con todos los secrets necesarios
+load_dotenv()
 ckey = os.environ.get('LC_C_KEY', None)
 privateKey = os.environ.get('LC_PRIVATE_KEY', None)
 convoID = os.environ.get('LC_CONVERSATION_ID', None)
 
+# Los datos necesarios para hacer un request a LC de una conversación en específico
 base_url = "https://api.liveconnect.chat/prod/"
-
 convo_endpoint = "history/conversation"
 convo_payload = { "id": convoID }
+convo_participants = {}
 
+# Regresa el token de autorización para LC
 def get_token():
   endpoint = "account/token"
   payload = {
@@ -41,6 +44,7 @@ def get_token():
   response = requests.post(base_url+endpoint, json=payload, headers=headers)
   return response.json()
 
+# Método general para requests a LC
 def get_liveconnect(lc_endpoint, lc_payload, token):
   endpoint = lc_endpoint
   payload = lc_payload
@@ -52,22 +56,51 @@ def get_liveconnect(lc_endpoint, lc_payload, token):
   response = requests.post(base_url+endpoint, json=payload, headers=headers)
   return response.json()
 
+# Cambia los IDs por los nombres de los usuarios e indica cuales son de parte del sistema
+def switch_contact_ids(dataframe):
+  for x in dataframe.index:
+    if dataframe.loc[x,'Interno'] == 1:
+      dataframe.drop(x, inplace=True)
+    else:
+      user_id = int(dataframe.loc[x,'Usuario'])
+      if user_id == 0:
+        dataframe.loc[x,'Usuario'] = 'Sistema'
+      else:
+        dataframe.loc[x,'Usuario'] = convo_participants[user_id]
+  dataframe.drop(['Interno'], axis=1, inplace=True)
+
 json_resp = get_token()
-os.environ['LC_PAGE_GEAR_TOKEN'] = json_resp['PageGearToken']
-pageGearToken = os.environ.get('LC_PAGE_GEAR_TOKEN', None)
+pageGearToken = json_resp['PageGearToken']
 print("\n"+json_resp['status_message']+"\n")
 
 json_resp = get_liveconnect(convo_endpoint, convo_payload, pageGearToken)
 print("\n"+json_resp['status_message']+"\n")
-if json_resp['status'] > 0:
-  print('ID: '+str(json_resp['data']['conversacion']['contacto']['id']))
-  print('Contacto: '+json_resp['data']['conversacion']['contacto']['nombre']+' '+json_resp['data']['conversacion']['contacto']['apellidos'])
-  print('Primer Mensaje: '+json_resp['data']['mensajes'][0]['mensaje'])
-  print('\n')
 
+# Guarda en un diccionario el nombre completo del prospecto o papá
+convo_participants[json_resp['data']['conversacion']['id_contacto']]=json_resp['data']['conversacion']['contacto']['nombre']
+convo_participants[json_resp['data']['conversacion']['id_contacto']]+=' '
+convo_participants[json_resp['data']['conversacion']['id_contacto']]+=json_resp['data']['conversacion']['contacto']['apellidos']
+
+# Guarda en un diccionario los datos de las ADM que le respondieron
+for x in json_resp['data']['participantes']:
+  convo_participants[x['id_usuario']] = x['nombre']
+
+# Acomoda los participantes y sus mensajes en una tabla que se exporta como .csv
+# El nombre del archivo es el tag del prospecto/papá y se guarda en el folder de su unidad
 if json_resp['status'] > 0:
   df = pd.json_normalize(json_resp['data']['mensajes'])
-  msg_data = df[['id_remitente', 'mensaje']]
+  convo_table = pd.DataFrame(columns=['Usuario','Mensaje','Fecha','Interno'])
+  convo_table['Usuario'] = df['id_remitente'].astype(object)
+  convo_table['Mensaje'] = df['mensaje'].astype(object)
+  convo_table['Fecha'] = df['fecha_add'].astype(object)
+  convo_table['Interno'] = df['interno']
+  switch_contact_ids(convo_table)
   path_name = '../data/convos/'+json_resp['data']['conversacion']['canalnombre']+'/'
   os.makedirs(path_name, exist_ok=True)
-  msg_data.to_csv(path_name+json_resp['data']['conversacion']['contacto']['nombre']+'.csv', encoding='utf-8-sig')
+  try:
+    convo_table.to_csv(path_name+json_resp['data']['conversacion']['contacto']['nombre']+'.csv', encoding='utf-8-sig')
+  except IOError as e:
+    print("\nHubo un error al crear el archivo: ", e)
+    if e.args[0] == 13: print("Por favor cierre el archivo y vuélvalo a intentar.\n")
+  else:
+    print("\nEl archivo se creó con éxito\n")
