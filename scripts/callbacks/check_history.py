97597188a7bb2bf96b362258a27079ab64415f18
Helena Ruiz <hruizrmz@gmail.com>
2025-08-26 19:36:30 -0600

    Edit contact info + dynamic greeting message
    + obtain data from contact
    + modify contact data (sucursal, contexto)
    + sucursal is detected by entry channel
    + greeting message changes based on available data and contact history
    
    pending: test new contact behavior, draft context based on convo history with AI

--- scripts/callbacks/check_history.py
+++ scripts/callbacks/check_history.py
@@ -6,10 +6,10 @@
 # Función: Determina el callback response al recibir un webhook de un chatbot. Por ahora, determina si el mensaje
 #           entrante es de un contacto nuevo o ya existente según su historial de conversaciones.
 #
-from xml.sax.handler import all_features
 from flask import Flask, request, jsonify
 import requests
 import os
+from enum_canales import Canales, Sucursales
 
 
 app = Flask(__name__)
@@ -32,31 +32,13 @@ def get_token():
 
 
 # Función para consultar historial de un contacto
-def get_contact_history(contact_id):
-    # Crea el token único por usuario para usar las APIs cada que corre el script
-    try:
-        token_json_resp = get_token()
-    except Exception as e:
-        print("HUBO UN ERROR AL GENERAR EL TOKEN")
-        print(f"{e}")
-        os.system("pause")
-    else:
-        pageGearToken = token_json_resp['PageGearToken']
-        print("¡TOKEN DE API OBTENIDO!")
-
-    # Pasamos el ID del contacto del cual queremos consultar el historial
-    # Este se obtiene del webhook lanzado al transferir la convo al equipo de chatbots
-    payload = {
-        "id_contacto": contact_id
-    }
-
+def get_liveconnect(endpoint, payload, pageGearToken):
     headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
         "PageGearToken": pageGearToken
     }
-
-    response = requests.post("https://api.liveconnect.chat/prod/history/conversations", json=payload, headers=headers)
+    response = requests.post("https://api.liveconnect.chat/prod"+endpoint, json=payload, headers=headers)
     return response.json()
 
 
@@ -69,6 +51,18 @@ def identify_contact():
         print(f"[WARNING] Secret inválido recibido: {incoming_secret}")
         return jsonify({"error": "Unauthorized"}), 401
     
+    # Crea el token único por usuario para usar las APIs cada que corre el script
+    pageGearToken = ""
+    try:
+        token_json_resp = get_token()
+    except Exception as e:
+        print("HUBO UN ERROR AL GENERAR EL TOKEN")
+        print(f"{e}")
+        os.system("pause")
+    else:
+        pageGearToken = token_json_resp['PageGearToken']
+        print("¡TOKEN DE API OBTENIDO!")
+    
     # Primero revisa si los datos recibidos continen la estructura correcta
     try:
         data = request.json
@@ -77,15 +71,64 @@ def identify_contact():
         print("Callback recibido:", data)
         
         # Ahora, extrae el id del contacto del mensaje entrante para revisar si tiene historial > 0
-        contact_id = data.get('chat', {}).get('contacto', {}).get('id_contacto')
-        respuesta = "texto respuesta"
+        contact_id = data.get('inputs', {}).get('id_contacto')
+        chat_id = data.get('chat', {}).get('id').strip()
+        historial = False
         if contact_id:
-            contact_history_json_resp = get_contact_history(contact_id)
-            if 'data' in contact_history_json_resp and len(contact_history_json_resp['data']) > 0:
-                respuesta = "Historial encontrado - Contacto existente"
+            # Pasamos el ID del contacto del cual queremos consultar el historial
+            # Este se obtiene del webhook lanzado al transferir la convo al equipo de chatbots
+            history_endpoint = "/history/conversations"
+            history_payload = { "id_contacto": contact_id }
+            contact_history_json_resp = get_liveconnect(history_endpoint, history_payload, pageGearToken)
+            if 'status' in contact_history_json_resp and contact_history_json_resp['status'] > 0:
+                historial = True
             else:
-                respuesta = "Historial no encontrado - Contacto nuevo"
-        print("Texto respuesta:", respuesta)
+                historial = False
+
+        valor_canal = data.get('chat', {}).get('id_canal', '')
+        sucursal = ""
+        if valor_canal:
+            tag_sucursal = Canales.from_value(valor_canal).name
+            print(tag_sucursal)
+            sucursal = Sucursales.from_value(tag_sucursal).value
+            print(sucursal)
+        else:
+            print("Error al solicitar la sucursal / nombre del canal")
+
+        if historial:
+            print("Historial encontrado - Contacto existente")
+            valor_contexto = data.get('inputs', {}).get('dinamicos', {}).get('dinamicoQBTVa1', '').strip()
+            contexto = "contexto de ejemplo"
+
+            if valor_contexto:
+                if valor_contexto == "":
+                    print("Contexto vacio")
+                    texto_respuesta = "¡Hola otra vez!"
+                else:
+                    edit_endpoint = "/contacts/edt"
+                    edit_payload = {
+                        "id": contact_id,
+                        "dinamicos": {
+                            "dinamicovmoXo1": sucursal,
+                            "dinamicoQBTVa1": contexto
+                        },
+                        "id_chat": chat_id
+                    }
+                    edit_contact_json_resp = get_liveconnect(edit_endpoint, edit_payload, pageGearToken)
+                    if 'status' in edit_contact_json_resp and edit_contact_json_resp['status'] < 0:
+                        print(edit_contact_json_resp['status_message'])
+                        texto_respuesta = "¡Hola otra vez!"
+                    else:
+                        print("Se editó el usuario")
+                        texto_respuesta = "Es un gusto saludarle de nuevo. Permítame confirmar lo siguiente:\n"
+                        texto_respuesta+="- Le interesa la sucursal "+sucursal+"\n"
+                        texto_respuesta+="- "+contexto+"\n"
+            else:
+                print("Error al obtener contexto del contacto")
+                texto_respuesta = "¡Hola otra vez!"
+        else:
+            print("Historial no encontrado - Contacto nuevo")
+            texto_respuesta = "¡Bienvenido a Froggin! Usted está contactando a la sucursal "+sucursal
 
         # Esta es la respuesta que se regresa a LC. La variable 'respuesta' varia.
         response = {
@@ -93,13 +136,14 @@ def identify_contact():
             "status_message": "Ok",
             "data": {
                 "actions": [
-                    {
-                        "type": "sendText",
-                        "text": respuesta
-                    }
+                {
+                    "type": "sendText",
+                    "text": texto_respuesta
+                }
                 ]
             }
         }
+
         return jsonify(response)
     except Exception as e:
         print(e)
---
+++ scripts/callbacks/enum_canales.py
@@ -0,0 +1,37 @@
+#
+# Versión 0.1
+# Fecha: 26 de agosto de 2025
+#
+# Autores: Helena Ruiz Ramírez
+# Función: Enumerador de los canales en LiveConnect por ID. Se utiliza para asignar la sucursal del contacto.
+#
+from enum import Enum
+
+
+class Canales(Enum):
+    ELI = ("115", "4124") # 02 Elite (WA), 02 Elite Facebook 
+    SAN = ("243", "19026") # 04 SanNicolas (WA), 04 SanNicolas Facebook
+    DOM = ("735", "19027") # 05 Dominio (WA), 05 Dominio Facebook
+    ESC = ("235", "19028") # 06 Escobedo (WA), 06 Escobedo Facebook
+    DEF = ()
+
+    @classmethod
+    def from_value(cls, value):
+        for member in cls:
+            if value in member.value:
+                return member
+        return Canales.DEF
+
+class Sucursales(Enum):
+    ELI = "Cumbres Elite"
+    SAN = "San Nicolás"
+    DOM = "Cumbres Dominio"
+    ESC = "Escobedo"
+    DEF = "Cumbres Elite"
+
+    @classmethod
+    def from_value(cls, value):
+        for member in cls:
+            if value in member.value:
+                return member
+        return Sucursales.DEF
